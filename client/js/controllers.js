4fb76b08dcb6e231ca506891ecb38d571e47c7e9
Ian Henderson <ianwhen@gmail.com>
2014-07-30 17:41:04 -0700

    Finished filtering functionality

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -11,14 +11,24 @@ angular.module('cantonAPI.controllers',[])
 })
 
 .controller('SearchCtrl', function($scope, requests) {
-  $scope.max = 0;
-  $scope.min = 0;
-  $scope.type = '';
+  $scope.max = '';
+  $scope.min = '';
+  $scope.answer = '';
 
   $scope.search = function(){
     requests.canton('all').then(function(data){
+      var min = $scope.min || 0;
+      var max = $scope.max || Infinity;
+      var answer = $scope.answer;
+      console.log(min, max, answer);
       console.log('data', data);
-      $scope.results = data;
+      $scope.results = data.filter(function(x){
+        if (max === Infinity && min === 0) {
+          return x;
+        } else {
+          return ((x[answer] < max) && (x[answer] > min));
+        }
+      });
     });
   };
 });
